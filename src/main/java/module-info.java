ee2aa24f5b49646bdcd7a19ac58fb4d33c00d245
Thomas Scheffler <github@yagee.de>
2019-01-02 13:21:19 +0100

    use java.util.logging as logging facade

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module org.mycore.imagetiler {
     requires java.xml;
     requires java.desktop;
+    requires java.logging;//no log4j: https://bugs.openjdk.java.net/browse/JDK-8208269
     requires java.xml.bind;
-    requires org.apache.logging.log4j;
+    requires jdk.zipfs;
     exports org.mycore.imagetiler;
     opens org.mycore.imagetiler to java.xml.bind;
     uses org.mycore.imagetiler.MCRTileEventHandler;
